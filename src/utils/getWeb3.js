ad9f2fb33c61a25fd8b4660cf470400f52d8f08d
Tomisin <tomisinjenrola@gmail.com>
2018-08-07 17:08:47 -0400

    Imported specified provider

--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -1,11 +1,12 @@
 import Web3 from 'aion-web3'
+import { host, port } from '../../titanrc'
 
 let getWeb3 = new Promise(function (resolve, reject) {
     // Wait for loading completion to avoid race conditions with web3 injection timing.
     window.addEventListener('load', function () {
         let results
         let web3
-        const provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545')
+        const provider = new Web3.providers.HttpProvider(`${host}:${port}`)
 
         web3 = new Web3(provider)
 
